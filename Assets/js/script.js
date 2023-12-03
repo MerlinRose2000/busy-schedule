4b9bb6c14d74531b5d49313eb24d462a43d6db2e
Camelot <em-rstewart@hotmail.com>
2023-12-03 16:49:01 -0400

    Made changes and updates to files to see if they would deploy

---
+++ Assets/js/script.js
@@ -0,0 +1,44 @@
+  $(document).ready(function () {
+
+    var displayTime = document.querySelector("#currentDay");
+  
+    var currentTime = dayjs().format("dddd, MMMM D, YYYY");
+  
+    displayTime.textContent = currentTime;
+  
+    $(".saveBtn").on("click", function () {
+      var text = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      localStorage.setItem(time, text);
+    });
+  
+    function hourTracker() {
+      var currentHour = dayjs().hour();
+    
+      $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+    hourTracker();
+  
+  function displayText() {
+    $(".time-block").each(function () {
+      var blockHour = $(this).attr("id");
+      $(this).children(".description").val(localStorage.getItem(blockHour));
+    });
+  }
+  displayText();
+  
+  });
